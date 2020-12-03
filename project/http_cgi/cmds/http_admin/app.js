59d3b05df6d06f27058fba8cb81f6e3358d67d77
Alex Kalmuk <alexkalmuk@gmail.com>
2020-12-03 12:07:59 +0300

    date

--- project/http_cgi/cmds/http_admin/app.js
+++ project/http_cgi/cmds/http_admin/app.js
@@ -22,10 +22,32 @@ angular.module("HttpAdmin", ['ngRoute', 'ui.bootstrap'])
         $http.post('cgi-bin/http_admin_backend', post_data);
     };
 }])
+.controller("DateCtrl", ['$scope', '$http', function($scope, $http) {
+    $scope.date = null;
+
+    $scope.save = function() {
+        //$http.get('cgi-bin/cgi_cmd_wrapper?c=flash_settings&a=store&a=led').success(function (data) {
+        //   alert('Led configuration saved!');
+        //});
+    };
+
+    $scope.update = function() {
+        $http.get('cgi-bin/cgi_cmd_wrapper?c=date').then(function (r) {
+            var data = r.data;
+            $scope.date = data;
+            //alert($scope.dates);
+        });
+    };
+
+    $scope.update();
+}])
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     when('/interfaces', {
         templateUrl: 'partials/interfaces.html',
+    }).
+    when('/date', {
+        templateUrl: 'partials/date.html',
     }).
     otherwise({
         redirectTo: '/interfaces'
